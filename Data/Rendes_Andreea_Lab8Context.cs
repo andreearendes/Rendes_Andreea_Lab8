eea7d487a0ae7e5b7972b1b9c169697a118e994d
Andreea Rendes <andreearendes@outlook.com>
2021-11-24 19:11:03 +0200

    Add project files.

---
+++ Data/Rendes_Andreea_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Rendes_Andreea_Lab8.Models;
+
+namespace Rendes_Andreea_Lab8.Data
+{
+    public class Rendes_Andreea_Lab8Context : DbContext
+    {
+        public Rendes_Andreea_Lab8Context (DbContextOptions<Rendes_Andreea_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Rendes_Andreea_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Rendes_Andreea_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Rendes_Andreea_Lab8.Models.Category> Category { get; set; }
+    }
+}
